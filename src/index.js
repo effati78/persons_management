a8692635620dd42d10099d429af144bcd309c82c
Reza <effati78@gmail.com>
2021-02-14 22:22:24 +0330

    The React-Toastify module was added to the project

--- src/index.js
+++ src/index.js
@@ -3,5 +3,7 @@ import { render } from 'react-dom';
 
 import App from './App';
 import './index.css';
+import 'react-toastify/dist/ReactToastify.css';
+
 
 render(<App />, document.getElementById('root'));
